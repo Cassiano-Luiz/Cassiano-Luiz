bada8c0894ca3069d7bf04a4c7073bd045153008
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-27 08:28:40 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,21 @@
+        fun main(args: Array<String>) {
+        var num = readLine()!!.toInt() {
+        println("digite um numero para descobrirmos o mês do ano.")
+        print("digite um valor entre 1 e 12 ")
+        var num = readLine()!!.toInt()
+        when (num) {
+
+        1 -> println("janeiro")
+        2 -> println("feveiro")
+        3 -> println("março")
+        4 -> println("abril")
+        5 -> println("maio")
+        6 -> println("junho")
+        7 -> println("julho")
+        8 -> println("agosto")
+        9 -> println("setembro")
+        10 -> println("outubro")
+        11 -> println("novembro")
+        12 -> println("dezembro")
+        else -> println("valor invalido")
+        }
